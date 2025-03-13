f424e500590bbee56ee5c93dbf66934a84d0b9f9
hquangdev <hoangquangdo245@gmail.com>
2025-03-13 11:28:11 +0700

    update search

--- Web-BE/src/main/java/com/example/webbe/Entity/ElasticSearch/Product.java
+++ Web-BE/src/main/java/com/example/webbe/Entity/ElasticSearch/Product.java
@@ -1,12 +1,41 @@
 package com.example.webbe.Entity.ElasticSearch;
 
 
+import jakarta.persistence.Id;
 import lombok.Data;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
 
 @Data
 @Document(indexName = "product")
 public class Product {
+    @Id
     private String id;
+
+    @Field(type = FieldType.Text)
     private String name;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Text)
+    private String image;
+
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    @Field(type = FieldType.Integer)
+    private int quantity;
+
+    @Field(type = FieldType.Integer)
+    private int status;
+
+    @Field(type = FieldType.Date)
+    private LocalDateTime createdAt;
+
+    @Field(type = FieldType.Date)
+    private LocalDateTime updatedAt;
 }
