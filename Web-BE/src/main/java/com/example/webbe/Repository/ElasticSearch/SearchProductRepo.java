7d232a4e9a9346808e3d1331a00d8fbde06c2410
hquangdev <hoangquangdo245@gmail.com>
2025-03-14 00:27:05 +0700

    Gộp 3 commit thành 1 commit duy nhất

--- Web-BE/src/main/java/com/example/webbe/Repository/ElasticSearch/SearchProductRepo.java
+++ Web-BE/src/main/java/com/example/webbe/Repository/ElasticSearch/SearchProductRepo.java
@@ -1,9 +1,13 @@
 package com.example.webbe.Repository.ElasticSearch;
 
 import com.example.webbe.Entity.ElasticSearch.ProductSearch;
+import com.example.webbe.Entity.Product;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SearchProductRepo extends ElasticsearchRepository<ProductSearch, String> {
+
+    //xóa lan 2
+    void delete(Product product);
 }
